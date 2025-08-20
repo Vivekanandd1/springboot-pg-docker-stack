1a777a62883333d9bd0727c4e7188b1319174e82
vivkekanandd1 <vivekanandd1@live.com>
2025-08-20 15:53:01 +0530

    Service Class to initiate Https request

--- src/main/java/com/vivekcode/TechEmployeeServices.java
+++ src/main/java/com/vivekcode/TechEmployeeServices.java
@@ -1,4 +1,19 @@
 package com.vivekcode;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TechEmployeeServices {
+
+    private TechEmployeeRepo techEmployeeRepo;
+
+    public TechEmployeeServices(TechEmployeeRepo techEmployeeRepo) {
+        this.techEmployeeRepo = techEmployeeRepo;
+    }
+
+    public List<TechEmployee> getTechEmployeeList() {
+        return techEmployeeRepo.findAll();
+    }
 }
